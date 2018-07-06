ff004abd32f03b4966994ee65e41d360f5db79ec
wangb <wangbx@kalamodo.com>
2018-07-06 16:48:38 +0800

    Initial commit

---
+++ eureka-client/src/main/java/test/spring/cloud/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package test.spring.cloud.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wangb
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
---
+++ eureka-server/src/main/java/test/spring/cloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package test.spring.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wangb
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
