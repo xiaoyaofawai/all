b1d9c7edbad8bb894405baa2a058cd7d200bceb1
wangb <wangbx@kalamodo.com>
2018-07-10 11:13:25 +0800

    修改

---
+++ eureka-feign-client/src/main/java/test/spring/cloud/eureka/feign/client/EurekaFeignClientApplication.java
@@ -0,0 +1,17 @@
+package test.spring.cloud.eureka.feign.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import test.spring.cloud.eureka.feign.client.config.FeignConfig;
+
+@EnableFeignClients
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaFeignClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaFeignClientApplication.class, args);
+    }
+}
